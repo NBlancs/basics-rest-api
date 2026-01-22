fcab2583be301a56f51b7e59a0008af2972237aa
NBlancs <bnoel.omar@gmail.com>
2026-01-22 22:18:25 +0800

    ProductController: added code in public function store

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -19,6 +19,13 @@ class ProductController extends Controller
     public function store(Request $request)
     {
         //
+
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'price' => 'required|numeric|min:0',
+            'quantity' => 'required|integer|min:0'
+        ]);
     }
 
     /**
