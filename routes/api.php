185cb940d9fe8a4b2ec1b46875ad0032efcfc91f
NBlancs <bnoel.omar@gmail.com>
2026-01-22 22:03:17 +0800

    feat: Set up basic REST API structure with Products resource

--- routes/api.php
+++ routes/api.php
@@ -9,7 +9,33 @@ use App\Http\Controllers\ProductController;
 //     return $request->user();
 // })->middleware('auth:sanctum');
 
+// BASIC REST API routes example sa products
 
-Route::get('/user', function(Request $request) {
-    return "Hello there I am api";
+Route::prefix('products')->group(function () {
+
+
+// GET /api/products - Get all products
+// similar to select * 
+Route::get('/', [ProductController::class, 'index']);
+
+// GET /api/products/{id}
+// Get single product
+Route::get('/{id}', [ProductController:: class, 'show']);
+
+// POST /api/products
+// Create new products
+Route::post('/', [ProductController:: class, 'store']);
+
+// PUT /api/products/{id}
+// Update product
+Route::put('/{id}', [ProductController:: class, 'update']);
+
+// DELETE /api/products/{id}
+// Delete product
+Route::delete('/{id}', [ProductController:: class, 'destroy']);
+
+
+// Shortcut daw sya
+// Route::apiResource('products', ProductController::class);
+
 });
